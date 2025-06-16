bfb834d0515a00be651e43f4b3b2c43087a56737
Meo <Meo@gmail.com>
2025-06-16 22:10:13 +0700

    Nhánh FE của Owner

--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
+// App.js
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import './App.css';
-import RouterAd from "./routes/AdminRoutes/RoutesAd";
-import HeaderComponent from './components/header/header';
-import FooterComponent from "./components/footer/footer";
+import RoutesOw from "./routes/OwnerRoutes/RoutesOw";
+
 function App() {
   return (
     <BrowserRouter>
-      <HeaderComponent />
-      <div style={{ marginTop: "80px" }}>
-        <RouterAd />
-      </div>
-      <FooterComponent />
+      <RoutesOw /> {/*Sử dụng route Owner */}
     </BrowserRouter>
   );
 }
 
 export default App;
+
+
+// import React from "react";
+// import { BrowserRouter } from "react-router-dom";
+// import './App.css';
+// import RouterAd from "./routes/AdminRoutes/RoutesAd";
+// import HeaderComponent from './components/header/header';
+// import FooterComponent from "./components/footer/footer";
+// function App() {
+//   return (
+//     <BrowserRouter>
+//       <HeaderComponent />
+//       <div style={{ marginTop: "80px" }}>
+//         <RouterAd />
+//       </div>
+//       <FooterComponent />
+//     </BrowserRouter>
+//   );
+// }
+
+// export default App;
---
+++ src/routes/OwnerRoutes/RoutesOw.jsx
@@ -0,0 +1,33 @@
+// File: src/routes/OwnerRoutes/RoutesOw.jsx
+import React from "react";
+import { Route, Routes, Navigate } from "react-router-dom";
+import OwnerLayout from "../../pages/OwnerPage/Components/Layout/layout";
+import Profile from "../../pages/OwnerPage/Profile/profile";
+import Accommodation from "../../pages/OwnerPage/Accommodation/accommodation";
+import Report from "../../pages/OwnerPage/Report/report";
+import Rating from "../../pages/OwnerPage/Rating/rating";
+import DetailRating from "../../pages/OwnerPage/Rating/DetailRating";
+import Communication from "../../pages/OwnerPage/Communication/communication";
+import Membership from "../../pages/OwnerPage/MemberShip/membership";
+import Cancellation from "../../pages/OwnerPage/Cancellation/cancellation";
+
+const RoutesOw = () => {
+    return (
+        <Routes>
+            <Route path="/" element={<Navigate to="/owner/profile" />} />
+
+            <Route path="/owner" element={<OwnerLayout />}>
+                <Route path="profile" element={<Profile />} />
+                <Route path="accommodation" element={<Accommodation />} />
+                <Route path="report" element={<Report />} />
+                <Route path="rating" element={<Rating />} />
+                <Route path="rating/:id" element={<DetailRating />} />
+                <Route path="communication" element={<Communication />} />
+                <Route path="membership" element={<Membership />} />
+                <Route path="cancellation" element={<Cancellation />} />
+            </Route>
+        </Routes>
+    );
+};
+
+export default RoutesOw;
